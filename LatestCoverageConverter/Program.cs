db1e677427480c44c9a03d94a6a5b32063d8b126
Echo Lee <bdottn.lee+git@gmail.com>
2018-03-25 16:14:13 +0800

    調整 Program 類別
    1.　加入方法：GenerateCoverageConverterArgument
    　1.1　依據傳入參數產生 CoverageConverterArgument 類別並填入相關屬性

--- LatestCoverageConverter/Program.cs
+++ LatestCoverageConverter/Program.cs
@@ -1,9 +1,54 @@
-﻿namespace LatestCoverageConverter
+﻿using System;
+using System.Linq;
+
+namespace LatestCoverageConverter
 {
     class Program
     {
         static void Main(string[] args)
         {
+            if (args == null && args.Length == 0)
+            {
+                throw new ArgumentNullException("參數錯誤：未傳入參數！");
+            }
+            else
+            {
+                var argument = GenerateCoverageConverterArgument(args);
+            }
+        }
+
+        private static CoverageConverterArgument GenerateCoverageConverterArgument(string[] args)
+        {
+            var argument = new CoverageConverterArgument();
+
+            foreach (var arg in args.Select(a => a.Split('=')))
+            {
+                switch (arg[0].ToLower())
+                {
+                    // Workspace Path
+                    case "-workspace":
+
+                        argument.Workspace = arg[1];
+
+                        break;
+
+                    // TestResultsFolder
+                    case "-testresultsfolder":
+
+                        argument.TestResultsFolder = arg[1];
+
+                        break;
+
+                    // Output FileName
+                    case "-outputfilename":
+
+                        argument.OutputFileName = arg[1];
+
+                        break;
+                }
+            }
+
+            return argument;
         }
     }
 }
