0b464edd1fef4213b65f903d0a98f2b1848e2e13
Echo Lee <bdottn.lee+git@gmail.com>
2018-03-25 16:41:12 +0800

    調整 Program 類別
    1.　加入方法：ConvertCoverage
    　1.1　取得 TestResults 目錄
    　1.2　取得 xml 輸出檔名
    　1.3　取得最新的 coverage 檔
    　1.4　轉換並重新命名最新的 coverage 檔
    
    2. 加入組件參考
    　2.1　Microsoft.VisualStudio.Coverage.Analysis.dll
    　2.2　Microsoft.VisualStudio.Coverage.Interop.dll
    
    3. 加入 MSTestCoverageToEmma.xsl

--- LatestCoverageConverter/Program.cs
+++ LatestCoverageConverter/Program.cs
@@ -1,5 +1,11 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.Coverage.Analysis;
+using System;
+using System.IO;
 using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Xml;
+using System.Xml.Xsl;
 
 namespace LatestCoverageConverter
 {
@@ -14,6 +20,8 @@ namespace LatestCoverageConverter
             else
             {
                 var argument = GenerateCoverageConverterArgument(args);
+
+                ConvertCoverage(argument);
             }
         }
 
@@ -49,6 +57,47 @@ namespace LatestCoverageConverter
             }
 
             return argument;
+        }
+
+        private static void ConvertCoverage(CoverageConverterArgument argument)
+        {
+            var testResultsDirectory = Path.Combine(argument.Workspace, argument.TestResultsFolder);
+
+            var targetFilePath = Path.Combine(testResultsDirectory, argument.OutputFileName);
+
+            if (Directory.Exists(testResultsDirectory) == false)
+            {
+                throw new DirectoryNotFoundException(string.Format("資料夾錯誤：資料夾【{0}】不存在！", testResultsDirectory));
+            }
+            else
+            {
+                var sourceCoverageFile = Directory.GetFiles(testResultsDirectory, "*.coverage", SearchOption.AllDirectories).OrderByDescending(f => File.GetLastWriteTime(f)).FirstOrDefault();
+
+                if (File.Exists(sourceCoverageFile))
+                {
+                    CoverageInfo coverageInfo = CoverageInfo.CreateFromFile(sourceCoverageFile);
+
+                    CoverageDS coverageDataSet = coverageInfo.BuildDataSet();
+
+                    using (XmlReader reader = new XmlTextReader(new StringReader(coverageDataSet.GetXml())))
+                    {
+                        using (XmlWriter writer = new XmlTextWriter(targetFilePath, Encoding.UTF8))
+                        {
+                            var transform = new XslCompiledTransform();
+
+                            transform.Load(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "MSTestCoverageToEmma.xsl"));
+
+                            transform.Transform(reader, writer);
+                        }
+                    }
+
+                    Console.WriteLine(string.Format("轉換檔案：【{0}】→【{1}】。", sourceCoverageFile, targetFilePath));
+                }
+                else
+                {
+                    throw new FileNotFoundException(string.Format("檔案錯誤：檔案【{0}】不存在！", sourceCoverageFile));
+                }
+            }
         }
     }
 }
