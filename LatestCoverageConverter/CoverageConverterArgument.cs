ceafbf528b882d9437e2edca953965629640e9f3
Echo Lee <bdottn.lee+git@gmail.com>
2018-03-25 16:11:05 +0800

    建立 CoverageConverterArgument 類別
    1.　加入屬性：Workspace
    2.　加入屬性：TestResultsFolder
    3.　加入屬性：OutputFileName

---
+++ LatestCoverageConverter/CoverageConverterArgument.cs
@@ -0,0 +1,11 @@
+﻿namespace LatestCoverageConverter
+{
+    sealed class CoverageConverterArgument
+    {
+        public string Workspace { get; set; }
+
+        public string TestResultsFolder { get; set; }
+
+        public string OutputFileName { get; set; }
+    }
+}
